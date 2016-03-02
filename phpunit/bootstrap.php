55bdbd09e15b2bb8a534bf3902365d6791269b11
Daryl Lozupone <daryl@clubdeuce.ninja>
2016-03-01 23:02:37 -0500

    Add tests for WPLib_Base

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -3,4 +3,10 @@ echo 'Welcome to the WPLib Test Suite' . PHP_EOL;
 echo 'Version 0.1' . PHP_EOL;
 echo 'Author: WPLib Team <team@wplib.org>' . PHP_EOL;
 
+define( 'WPLIB_TESTS_WORKING_DIR', dirname( __DIR__ ) );
+
+require WPLIB_TESTS_WORKING_DIR . '/vendor/wp-tests/trunk/tests/phpunit/includes/functions.php';
+require WPLIB_TESTS_WORKING_DIR . '/vendor/wp-tests/trunk/tests/phpunit/includes/bootstrap.php';
+
 require 'framework/test-case.php';
+require WPLIB_TESTS_WORKING_DIR . '/vendor/wplib/wplib/wplib.php';
--- phpunit/framework/test-case.php
+++ phpunit/framework/test-case.php
@@ -1,6 +1,43 @@
 <?php
-namespace Tests\WPLib\Framework;
 
-class WPLib_Test_Case extends \PHPUnit_Framework_TestCase {
+namespace Tests\WPLib {
+
+    use \WP_UnitTestCase;
+
+    class WPLib_Test_Case extends WP_UnitTestCase {
+
+        public function getReflectionPropertyValue( $class, $property ) {
+
+            $reflection = new \ReflectionProperty( $class, $property );
+            $reflection->setAccessible( true );
+            return $reflection->getValue( $class );
+
+        }
+
+        public function setReflectionPropertyValue( $class, $property, $value ) {
+
+            $reflection = new \ReflectionProperty( $class, $property );
+            $reflection->setAccessible( true );
+            return $reflection->setValue( $class, $value );
+
+        }
+
+        public function reflectionMethodInvoke( $class, $method ) {
+
+            $reflection = new \ReflectionMethod( $class, $method );
+            $reflection->setAccessible( true );
+            $reflection->invoke( $class );
+
+        }
+
+        public function reflectionMethodInvokeArgs( $class, $method, $args ) {
+
+            $reflection = new \ReflectionMethod( $class, $method );
+            $reflection->setAccessible( true );
+            $reflection->invoke( $class, $args );
+
+        }
+
+    }
 
 }
