f5f312440271d1e640e3644e648f7ef8bba6dbcb
Daryl Lozupone <daryl@clubdeuce.ninja>
2016-03-02 21:54:02 -0500

    Add environment variables to bootstrap

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -3,18 +3,27 @@ echo 'Welcome to the WPLib Test Suite' . PHP_EOL;
 echo 'Version 0.1' . PHP_EOL;
 echo 'Author: WPLib Team <team@wplib.org>' . PHP_EOL;
 
-$working = getenv( 'WPLIB_TESTS_WORKING_DIR' );
+$working    = getenv( 'WPLIB_TESTS_WORKING_DIR' );
+$wp_tests = getenv( 'WP_TESTS_DIR' );
 
 if ( isset( $working ) && '' != $working ) {
     define( 'WPLIB_TESTS_WORKING_DIR', $working );
+}
+
+if (  ! empty( $wp_tests ) ) {
+    define( 'WP_TESTS_DIR', $wp_tests );
 }
 
 if( ! defined( 'WPLIB_TESTS_WORKING_DIR' ) ) {
     define( 'WPLIB_TESTS_WORKING_DIR', dirname( __DIR__ ) );
 }
 
-require WPLIB_TESTS_WORKING_DIR . '/vendor/wp-tests/tags/4.4.2/tests/phpunit/includes/functions.php';
-require WPLIB_TESTS_WORKING_DIR . '/vendor/wp-tests/tags/4.4.2/tests/phpunit/includes/bootstrap.php';
+if( ! defined( 'WP_TESTS_DIR' ) ) {
+    define( 'WP_TESTS_DIR',  WPLIB_TESTS_WORKING_DIR . '/vendor/wp-tests/trunk');
+}
+
+require WP_TESTS_DIR . '/tests/phpunit/includes/functions.php';
+require WP_TESTS_DIR . '/tests/phpunit/includes/bootstrap.php';
 
 require WPLIB_TESTS_WORKING_DIR . '/vendor/wplib/tests-wplib/phpunit/framework/test-case.php';
 require WPLIB_TESTS_WORKING_DIR . '/wplib.php';
